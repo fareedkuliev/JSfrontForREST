8e75e97b80e5263677ec0e988f253a32e58b2335
Farid Kuliev <f.farenco@gmail.com>
2023-05-13 23:55:48 +0400

    Updating main js file - added new functions and front and back connection completed. Added styles and some new forms into index.html

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// let id = null;
 async function getPosts(){
     let res = await fetch('http://localhost:8080/posts');
     let data = await res.json();
@@ -8,14 +9,14 @@ async function getPosts(){
             <div class="card-body">
                 <h5 class="card-title">${dataPost.title}</h5>
                 <p class="card-text"> ${dataPost.description}</p>
-                <a href="#" class="card-link"> Click her </a>
+                <a href="#" class="card-link" onclick="selectData('${dataPost.id}', '${dataPost.title}', '${dataPost.description}')"> Post updating </a>
+                <a href="#" class="card-link" onclick="deletePost(${dataPost.id})"> Delete </a>
             </div>
         </div>
 `
     })
     console.log(data);
 }
-getPosts();
 
 async function addPost(){
     const inputTitle = document.getElementById('inputTitle').value;
@@ -32,8 +33,45 @@ async function addPost(){
 
     const data = response.json();
 
-    // if (data.status === true){
-    //     await getPosts();
-    // }
+    if (data.status === true){
+        location.reload();
+    }
+}
 
+getPosts();
+
+async function deletePost(id){
+    const response = await fetch(`http://localhost:8080/deletepost/${id}`, {
+        method: "DELETE"
+    })
+const data = await response.json()
+    if(data.status === true){
+     location.reload();
+    }
+}
+
+async function selectData(id, title, description){
+    document.getElementById('inputID').value = id;
+    document.getElementById('inputTitleToUpdate').value = title;
+    document.getElementById('inputDescriptionToUpdate').value = description;
+}
+async function updatePost(){
+
+    const id = document.getElementById('inputID').value;
+    const title = document.getElementById('inputTitleToUpdate').value;
+    const description = document.getElementById('inputDescriptionToUpdate').value;
+    const dataToUpdate = {
+        "title": `${title}`,
+        "description": `${description}`
+    }
+    const response = await fetch(`http://localhost:8080/updatepost/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify(dataToUpdate)
+    })
+
+    const data = await response.json()
+
+    if(data.status === true){
+        location.reload();
+    }
 }
